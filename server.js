466d41034cf07183584c3a33e73f0bc934b0bb07
gregoriusjimmy <gregoriusjimmy@gmail.com>
2019-11-05 07:19:55 +0700

    changing response

--- server.js
+++ server.js
@@ -24,6 +24,10 @@ const app = express()
 app.use(bodyParser.json())
 app.use(cors())
 
+app.get('/', (req, res) => {
+  res.send('it is working')
+})
+
 app.post('/signin', (req, res) => {
   signin.handleSignin(req, res, db, bcrypt)
 })
@@ -44,6 +48,6 @@ app.post('/imageurl', (req, res) => {
   image.handleApiCall(req, res)
 })
 
-app.listen(process.env.PORT || 3000, () => {
+app.listen(process.env.PORT, () => {
   console.log(`app is running on port ${process.env.PORT}`)
 })
